e53c89df5d2a960a13dddfd1ceeaaf190d1b02ca
JPaul <jaysonpaul@gmail.com>
2017-06-20 19:55:03 -0400

    Adding instructions Markdown and updating gitignore

--- src/package-indexer.rb
+++ src/package-indexer.rb
@@ -6,7 +6,7 @@ require 'pstore'
 server = TCPServer.open(8080)
 loop do
   Thread.start(server.accept) do |client|
-    client.puts(Time.now.ctime) # Send the time to the client
-    client.close                # Disconnect from the client
+    client.puts(Time.now.ctime)
+    client.close
   end
 end
