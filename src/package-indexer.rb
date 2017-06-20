0190c192063faaa4db074632a3c889b747fad6e8
JPaul <jaysonpaul@gmail.com>
2017-06-20 19:45:39 -0400

    Initial rakefile for tests and rubocop

--- src/package-indexer.rb
+++ src/package-indexer.rb
@@ -1,13 +1,12 @@
 #!/usr/bin/env ruby
 
-
 require 'socket'
+require 'pstore'
 
 server = TCPServer.open(8080)
-loop {
+loop do
   Thread.start(server.accept) do |client|
     client.puts(Time.now.ctime) # Send the time to the client
-    client.puts "Closing the connection. Bye!"
     client.close                # Disconnect from the client
   end
-}
+end
